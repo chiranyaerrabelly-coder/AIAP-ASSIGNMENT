28a2a54a14f1d7f0e23da16b7a3e55eacd0b3ca9
chiranyaerrabelly-coder <chiranyaerrabelly@gmail.com>
2025-11-25 19:18:17 +0530

    TASK4.4.py

---
+++ ASSIGNMENT4/TASK4.4.py
@@ -0,0 +1,12 @@
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    name = input("Enter a string: ")
+    print("Number of vowels:", count_vowels(name))
