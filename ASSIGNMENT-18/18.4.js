4d6f960120a650b6a8361a1160377e858dda6f61
chiranyaerrabelly-coder <chiranyaerrabelly@gmail.com>
2025-12-10 23:03:26 +0530

    18.4.js

---
+++ ASSIGNMENT-18/18.4.js
@@ -0,0 +1,9 @@
+function printStudents(students) {
+    console.log("Student List:");
+    for (let i = 0; i < students.length; i++) {
+        console.log(students[i]);
+    }
+}
+
+const students = ["Alice", "Bob", "Charlie"];
+printStudents(students);
